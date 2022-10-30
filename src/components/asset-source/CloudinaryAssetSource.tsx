40e671e6beb3ab98cf7622c1eeaf722d092eea2c
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-10-30 21:51:06 +0100

    fix: callbacks in CloudinaryAssetSource should no longer be stale

--- src/components/asset-source/CloudinaryAssetSource.tsx
+++ src/components/asset-source/CloudinaryAssetSource.tsx
@@ -14,12 +14,12 @@ export const Widget = styled.div`
 `
 
 export function CloudinaryAssetSource(props: AssetSourceComponentProps) {
-  const {selectedAssets, selectionType, onSelect, onClose} = props
+  const {onClose} = props
 
   const [loadingMessage, setLoadingMessage] = useState<string | undefined>(
     'Loading Cloudinary Media Libary'
   )
-  const [library, setLibrary] = useState<CloudinaryMediaLibrary | undefined>()
+  const library = useRef<CloudinaryMediaLibrary | undefined>()
   const contentRef = useRef<HTMLDivElement | null>(null)
   const {secrets} = useSecrets<Secrets>(namespace)
   const cloudName = secrets?.cloudName
@@ -27,75 +27,18 @@ export function CloudinaryAssetSource(props: AssetSourceComponentProps) {
   const [widgetId] = useState(() => `cloundinaryWidget-${Date.now()}`)
   const [showSettings, setShowSettings] = useState(false)
 
-  const setupMediaLibrary = useCallback(
-    (lib: CloudinaryMediaLibrary) => {
-      setLibrary(lib)
-      const firstSelectedAsset = selectedAssets ? selectedAssets[0] : null
+  const propsRef = useRef(props)
 
-      // eslint-disable-next-line no-undef
-      const iframe: ChildNode | null | undefined =
-        contentRef.current && contentRef.current.firstChild
-      if (iframe && iframe instanceof HTMLIFrameElement) {
-        setLoadingMessage(undefined)
-        let asset
-        if (
-          selectionType === 'single' &&
-          firstSelectedAsset &&
-          firstSelectedAsset.source &&
-          firstSelectedAsset.source.id
-        ) {
-          asset = decodeSourceId(firstSelectedAsset.source.id)
-        }
-        const folder = asset
-          ? {
-              path: asset.public_id.split('/').slice(0, -1).join('/'),
-              resource_type: 'image',
-            }
-          : {path: '', resource_type: 'image'}
-        if (lib && contentRef.current) {
-          lib.show({folder, asset})
-          contentRef.current.style.visibility = 'visible'
-        }
-      }
-    },
-    [setLibrary, selectedAssets, selectionType]
-  )
-
-  const handleSelect = useCallback(
-    ({assets}: InsertHandlerParams) => {
-      if (!library) {
-        return
-      }
-      const imageAssets = assets.filter((asset) => asset.resource_type === 'image')
-      if (imageAssets.length === 0) {
-        throw new Error('The selection did not contain any images.')
-      }
-      library.hide()
-      onSelect(
-        imageAssets.map((asset) => {
-          const url =
-            asset.derived && asset.derived[0] ? asset.derived[0].secure_url : asset.secure_url
-          return {
-            kind: 'url',
-            value: url,
-            assetDocumentProps: {
-              _type: 'sanity.imageAsset',
-              originalFilename: encodeFilename(asset),
-              source: {
-                id: encodeSourceId(asset),
-                name: `cloudinary:${cloudName}`,
-              },
-            } as ImageAsset,
-          }
-        })
-      )
-    },
-    [onSelect, library, cloudName]
-  )
+  useEffect(() => {
+    // because we have to access props after loading js in a callback,
+    // we cannot pass props as dependecnies as that will cause infinite updates
+    // this takes a snapshot of props, so we can access them later
+    propsRef.current = props
+  }, [props])
 
   const handleClose = useCallback(() => {
-    if (library) {
-      library.hide()
+    if (library.current) {
+      library.current.hide()
     }
     onClose()
   }, [onClose, library])
@@ -109,18 +52,69 @@ export function CloudinaryAssetSource(props: AssetSourceComponentProps) {
       cloudName,
       apiKey,
       inlineContainer: `#${widgetId}`,
-      libraryCreated: setupMediaLibrary,
-      insertHandler: handleSelect,
+      libraryCreated: (lib: CloudinaryMediaLibrary) => {
+        library.current = lib
+        const selectedAssets = propsRef.current.selectedAssets
+        const firstSelectedAsset = selectedAssets ? selectedAssets[0] : null
+
+        // eslint-disable-next-line no-undef
+        const iframe: ChildNode | null | undefined =
+          contentRef.current && contentRef.current.firstChild
+        if (iframe && iframe instanceof HTMLIFrameElement) {
+          setLoadingMessage(undefined)
+          let asset
+          if (
+            propsRef.current.selectionType === 'single' &&
+            firstSelectedAsset &&
+            firstSelectedAsset.source &&
+            firstSelectedAsset.source.id
+          ) {
+            asset = decodeSourceId(firstSelectedAsset.source.id)
+          }
+          const folder = asset
+            ? {
+                path: asset.public_id.split('/').slice(0, -1).join('/'),
+                resource_type: 'image',
+              }
+            : {path: '', resource_type: 'image'}
+          if (lib && contentRef.current) {
+            lib.show({folder, asset})
+            contentRef.current.style.visibility = 'visible'
+          }
+        }
+      },
+      insertHandler: ({assets}: InsertHandlerParams) => {
+        if (!library.current) {
+          return
+        }
+        const imageAssets = assets.filter((asset) => asset.resource_type === 'image')
+        if (imageAssets.length === 0) {
+          throw new Error('The selection did not contain any images.')
+        }
+        library.current.hide()
+        propsRef.current.onSelect(
+          imageAssets.map((asset) => {
+            const url =
+              asset.derived && asset.derived[0] ? asset.derived[0].secure_url : asset.secure_url
+            return {
+              kind: 'url',
+              value: url,
+              assetDocumentProps: {
+                _type: 'sanity.imageAsset',
+                originalFilename: encodeFilename(asset),
+                source: {
+                  id: encodeSourceId(asset),
+                  name: `cloudinary:${cloudName}`,
+                },
+              } as ImageAsset,
+            }
+          })
+        )
+      },
     })
+  }, [cloudName, apiKey, widgetId])
 
-    // eslint-disable-next-line consistent-return
-    return () => {
-      setLibrary(undefined)
-    }
-    //we only want this to fire when secrets change
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cloudName, apiKey])
-
+  const hasConfig = apiKey && cloudName
   return (
     <Dialog
       id="cloudinary-asset-source"
@@ -139,10 +133,11 @@ export function CloudinaryAssetSource(props: AssetSourceComponentProps) {
             title="Configure"
             onClick={() => setShowSettings(true)}
             tabIndex={1}
+            text={hasConfig ? undefined : 'Configure Cloudinary plugin'}
           />
         </Flex>
 
-        {loadingMessage && (
+        {hasConfig && loadingMessage && (
           <Stack space={3}>
             <Flex align="center" justify="center">
               <Spinner muted />
