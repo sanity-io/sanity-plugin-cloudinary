780e0eb53eaeb3fec270d3edb98f662958a0415b
ankit <codertacina@gmail.com>
2025-03-28 02:38:54 +0530

    fix: pdf preview

--- src/components/AssetPreview.tsx
+++ src/components/AssetPreview.tsx
@@ -3,17 +3,37 @@ import VideoPlayer from './VideoPlayer'
 import {assetUrl} from '../utils'
 import {Flex} from '@sanity/ui'
 import {CloudinaryAsset} from '../types'
+import {useSecrets} from '@sanity/studio-secrets'
+import {namespace} from './SecretsConfigView'
 
 interface ComponentProps {
   layout?: 'default' | 'block'
   value: CloudinaryAsset | undefined
 }
 
+interface ApiConfig {
+  cloudName: string
+}
+
 const AssetPreview = ({value, layout}: ComponentProps) => {
+  const {secrets, loading} = useSecrets<ApiConfig>(namespace)
+
   const url = value && assetUrl(value)
   if (!value || !url) {
     return null
   }
+
+  if (value.format === 'pdf' && !loading && !secrets?.cloudName) {
+    console.error('Cloudinary cloudName is missing')
+    return null
+  }
+
+  const previewUrl =
+    value.format === 'pdf' && !loading
+      ? `https://res.cloudinary.com/${secrets?.cloudName ?? ''}/image/upload/f_jpg,pg_1/${
+          value.public_id
+        }.${value.format}`
+      : url
 
   switch (value.resource_type) {
     case 'video':
@@ -24,7 +44,7 @@ const AssetPreview = ({value, layout}: ComponentProps) => {
             maxWidth: layout === 'default' ? '80px' : '100%',
           }}
         >
-          <VideoPlayer src={url} kind="player" />
+          <VideoPlayer src={previewUrl} kind="player" />
         </Flex>
       )
     default:
@@ -32,7 +52,7 @@ const AssetPreview = ({value, layout}: ComponentProps) => {
         <Flex align="center">
           <img
             alt="preview"
-            src={url}
+            src={previewUrl}
             style={{
               maxWidth: layout === 'default' ? '80px' : '100%',
               height: 'auto',
