339bc563f1d51c66b3a6e42fd60bc4d69d95f301
Ankit Singh <codertacina@gmail.com>
2025-04-17 00:32:57 +0530

    feat: pdf and raw resource_type preview

--- src/components/AssetPreview.tsx
+++ src/components/AssetPreview.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import VideoPlayer from './VideoPlayer'
 import {assetUrl} from '../utils'
-import {Flex} from '@sanity/ui'
+import {Flex, Text} from '@sanity/ui'
 import {CloudinaryAsset} from '../types'
+import {DocumentIcon} from '@sanity/icons'
 
 interface ComponentProps {
   layout?: 'default' | 'block'
@@ -27,12 +28,31 @@ const AssetPreview = ({value, layout}: ComponentProps) => {
           <VideoPlayer src={url} kind="player" />
         </Flex>
       )
+    case 'raw':
+      return (
+        <Flex align="center">
+          <DocumentIcon />
+          <Text size={1} style={{marginLeft: '0.5em'}}>
+            {value.display_name ?? 'Raw file'}
+          </Text>
+        </Flex>
+      )
     default:
       return (
         <Flex align="center">
           <img
             alt="preview"
-            src={url}
+            src={
+              // Cloudinary returns resource_type as "image" even for PDFs,
+              // so we check the format to handle PDFs specifically.
+              // If it's a PDF, convert the first page to JPG and overlay a "PDF" label for thumbnail clarity.
+              value.format === 'pdf'
+                ? url?.replace(
+                    'image/upload',
+                    'image/upload/f_jpg,pg_1,l_text:Verdana_75_letter_spacing_14:PDF'
+                  )
+                : url
+            }
             style={{
               maxWidth: layout === 'default' ? '80px' : '100%',
               height: 'auto',
